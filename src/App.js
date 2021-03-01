2450c6a2fb094044ad1f0c8777e9c0d317f2ad8b
DESKTOP-2A54VHJ\user <jackal132@naver.com>
2021-03-01 16:05:39 +0900

    ajax로 컴포넌트 상태 변경하기

--- src/App.js
+++ src/App.js
@@ -21,9 +21,15 @@ class Nav extends Component {
 
   render() {
     var listTag = [];
-    for(var i =0; i < this.state.list.length; i++){
+    for(var i =0; i < this.state.list.length; i++) {
       var li = this.state.list[i];
-      listTag.push(<li key={li.id}><a href={li.id}>{li.title}</a></li>)
+      listTag.push(
+        <li key={li.id}>
+          <a href={li.id} data-id={li.id} onClick={function(e) {
+              e.preventDefault();
+              this.props.onClick(e.target.dataset.id);
+            }.bind(this)}>{li.title}</a>
+        </li>)
     }
 
     return (
@@ -36,17 +42,47 @@ class Nav extends Component {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <h1>WEB</h1>
-      <Nav></Nav>
+class Article extends Component {
+  render() {
+    return (
       <article>
-        <h2>Welcome</h2>
-        Hello, React &amp; Ajax
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
       </article>
-    </div>
-  );
+    );
+  }
+}
+
+class App extends Component {
+
+  state = {
+    article:{title:'Welcome', desc:'Hello, React & Ajax'}
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <h1>WEB</h1>
+        <Nav onClick={function(id) {
+          fetch(id+'.json')
+          .then(function(result){
+            return result.json();
+          }).then(function(json){
+            this.setState({
+              article:{
+                title:json.title,
+                desc:json.desc
+              }
+            });
+          }.bind(this));
+        }.bind(this)}></Nav>
+        <Article 
+          title={this.state.article.title} 
+          desc={this.state.article.desc}>
+        </Article>
+      </div>
+    )
+  }
 }
 
 export default App;
