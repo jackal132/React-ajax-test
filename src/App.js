2b5da3cece4a6e570ab438bb35ddfad8f1794405
DESKTOP-2A54VHJ\user <jackal132@naver.com>
2021-03-01 16:29:32 +0900

    로딩 중 기능 구현

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class Nav extends Component {
     var listTag = [];
     for(var i =0; i < this.props.list.length; i++) {
       var li = this.props.list[i];
-      console.log(li);
+      
       listTag.push(
         <li key={li.id}>
           <a href={li.id} data-id={li.id} onClick={function(e) {
@@ -39,47 +39,86 @@ class Article extends Component {
   }
 }
 
+class NowLoading extends Component {
+  render() {
+    return (<div>NowLoading...</div>);
+  }
+}
+
 class App extends Component {
 
   state = {
-    article:{title:'Welcome', desc:'Hello, React & Ajax'},
-    list:[]
+    article:{
+      item:{title:'Welcome', desc:'Hello, React & Ajax'},
+      isLoading:false
+    },
+    list:{
+      items:[],
+      isLoading:false
+    }
   }
 
   // 컴포넌트가 생성될때 ajax call 을 통해서 초기화를 해야될경우에
   // componentDidMount()를 ajax call을 넣고 ajax로 가져온 데이터로 직접
   // 영향을 주는것이 아니라 state를 통해 render가 영향을 받도록 한다.
   componentDidMount() {
+    
+    var newList = Object.assign({}, this.state.list, {isLoading:true});
+    this.setState({list:newList});
+
     fetch('list.json')
       .then(function(result) {
         return result.json();
       })
       .then(function(json) {
-        this.setState({list:json});
+        this.setState({list:{
+          items:json,
+          isLoading:false
+        }});
       }.bind(this));  
   }
 
   render () {
+    var NavTag = null;
+    if(this.state.list.isLoading) {
+      NavTag = <NowLoading></NowLoading>
+    } else {
+      NavTag = <Nav list={this.state.list.items} onClick={function(id) {
+        var newArticle = Object.assign({}, this.state.article, {isLoading:true});
+        this.setState({article:newArticle});
+
+        fetch(id+'.json')
+        .then(function(result){
+          return result.json();
+        }).then(function(json){
+          this.setState({
+            article:{
+              item:{
+                title:json.title,
+                desc:json.desc
+              },
+              isLoading:false
+            }
+          });
+        }.bind(this));
+      }.bind(this)}></Nav>
+    }
+
+    var ArticleTag = null;
+    if(this.state.article.isLoading){
+      ArticleTag = <NowLoading></NowLoading>;
+    } else {
+      ArticleTag = <Article 
+        title={this.state.article.item.title} 
+        desc={this.state.article.item.desc}>
+      </Article>;
+    }
+
     return (
       <div className="App">
         <h1>WEB</h1>
-        <Nav list={this.state.list} onClick={function(id) {
-          fetch(id+'.json')
-          .then(function(result){
-            return result.json();
-          }).then(function(json){
-            this.setState({
-              article:{
-                title:json.title,
-                desc:json.desc
-              }
-            });
-          }.bind(this));
-        }.bind(this)}></Nav>
-        <Article 
-          title={this.state.article.title} 
-          desc={this.state.article.desc}>
-        </Article>
+        {NavTag}
+        {ArticleTag}
       </div>
     )
   }
